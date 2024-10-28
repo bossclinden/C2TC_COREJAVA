812099df87c706fc60f48dd59c6799f5c8c65d32
WELCOME <WELCOME@DESKTOP-D4TCOO9>
2024-10-28 10:03:50 +0530

    day 2

---
+++ C2TC_BOSS_CLINDEN_A/src/day2/IdentifersDemo.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Akash"; //use camelcase
+		System.out.println("value is : "+ studentName);
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
