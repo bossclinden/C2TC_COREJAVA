a20d5292b14eda9b7388d9ce8b4e5e1e648a6590
WELCOME <WELCOME@DESKTOP-D4TCOO9>
2024-11-06 10:42:51 +0530

    BOSS

---
+++ C2TC_BOSS_CLINDEN_A/src/day3/inheri/SLInheritanceDemo.java
@@ -0,0 +1,10 @@
+package day3.inheri;
+
+public class SLInheritanceDemo {
+	public static void main(String[] args) {
+		Student student = new Student("BOSSCLINDEN", "8080808080", "CHENNAI", 9888878787L, 10, "City College");
+		System.out.println(student);
+		}
+
+
+}
