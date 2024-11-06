0e06b76218835454e88108d7523f2e9068cb7556
WELCOME <WELCOME@DESKTOP-D4TCOO9>
2024-11-06 10:28:56 +0530

    BOSS

---
+++ C2TC_BOSS_CLINDEN_A/src/day3/Customer/Customer.java
@@ -0,0 +1,36 @@
+package day3.Customer;
+
+public class Customer {
+	private String customerName;
+	private int customerId;
+	private String customerCity;
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerName=" + customerName + ", customerId=" + customerId + ", customerCity="
+				+ customerCity + "]";
+	}
+	public Customer() {
+		super();
+		this.customerName = customerName;
+		this.customerId = customerId;
+		this.customerCity = customerCity;
+	}
+}
---
+++ C2TC_BOSS_CLINDEN_A/src/day3/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj = new
+				OopsConceptDemo();
+				obj.setSerialNum(4211);
+				obj.setName("BOSSCLINDEN");
+				obj.setAge(20);
+				System.out.println(obj);
+
+
+	}
+
+}
