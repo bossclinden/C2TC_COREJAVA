ea12b0d3cefc3a10638e0cce9a533baf28c90fcf
WELCOME <WELCOME@DESKTOP-D4TCOO9>
2024-11-06 15:53:48 +0530

    BOSS

---
+++ C2TC_BOSS_CLINDEN_A/src/day6/fina/FinalClass.java
@@ -0,0 +1,12 @@
+package day6.fina;
+
+	final class FinalClass {
+		void show() {
+			System.out.println("Final class cannot be inherited");
+		}
+	}
+
+	//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
+
+
+	
